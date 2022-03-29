af12bb609cd8e914f6d258c0059dfad0f5b358f0
Diego Brito <britodiego.tech@gmail.com>
2022-03-29 04:41:43 -0400

    update

--- src/AntiDump.sol
+++ src/AntiDump.sol
@@ -1,4 +1,15 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract AntiDump {}
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+//import "@openzeppelin/contracts/access/"
+
+contract AntiDump is Ownable {
+    IERC20 private immutable s_token;
+
+    constructor(address _token) payable {
+        s_token = IERC20(_token);
+    }
+}
